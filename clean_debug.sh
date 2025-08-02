859004322ff0245a59cf683574540ab3278c3b64
xit303 <d.lange303@gmail.com>
2025-08-02 11:39:28 +0200

    feat: Add build and clean scripts for the xit project

---
+++ clean_debug.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# DEBUG
+
+# This script builds the complete xit project using xmake.
+# It assumes that xmake is installed and available in the PATH.
+# It builds the project in its subdirectories.
+# It also assumes that the xmakefile.json files are present in each subdirectory.
+
+# The order of the build is important, as all projects depend on others (except xit).
+
+echo "*********cleaning xit*********"
+xmake xit/xmakefile.json clean
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit. Exiting."
+    exit 1
+fi
+
+echo "*********cleaning xit.Timers*********"
+xmake xit.Timers/xmakefile.json clean
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Timers. Exiting."
+    exit 1
+fi
+
+echo "*********cleaning xit.IO*********"
+xmake xit.IO/xmakefile.json clean
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.IO. Exiting."
+    exit 1
+fi
+
+echo "*********cleaning xit.Input*********"
+xmake xit.Input/xmakefile.json clean
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Input. Exiting."
+    exit 1
+fi
+
+echo "*********cleaning xit.Clipboard*********"
+xmake xit.Clipboard/xmakefile.json clean
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Clipboard. Exiting."
+    exit 1
+fi
+
+echo "*********cleaning xit.Security*********"
+xmake xit.Security/xmakefile.json clean
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Security. Exiting."
+    exit 1
+fi
+
+echo "*********cleaning xit.Threading*********"
+xmake xit.Threading/xmakefile.json clean
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Threading. Exiting."
+    exit 1
+fi
+
+echo "*********cleaning xit.Application*********"
+xmake xit.Application/xmakefile.json clean
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Application. Exiting."
+    exit 1
+fi
+
+echo "*********cleaning xit.Account*********"
+xmake xit.Account/xmakefile.json clean
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Account. Exiting."
+    exit 1
+fi
+
+echo "*********cleaning xit.Drawing*********"
+xmake xit.Drawing/xmakefile.json clean
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Drawing. Exiting."
+    exit 1
+fi
