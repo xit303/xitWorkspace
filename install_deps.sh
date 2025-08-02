5f820940952611b7461901048c7f65b30a80466b
xit303 <d.lange303@gmail.com>
2025-08-02 12:13:37 +0200

    feat: Add installation script to streamline dependency setup for all modules

---
+++ install_deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./xit/install_deps.sh
+./xit.Timers/install_deps.sh
+./xit.Security/install_deps.sh
+./xit.IO/install_deps.sh
+./xit.Threading/install_deps.sh
+./xit.Application/install_deps.sh
+./xit.Account/install_deps.sh
