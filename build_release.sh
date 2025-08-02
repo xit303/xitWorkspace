96784eb4f1c1157121998acef33c7890fefe310b
xit303 <d.lange303@gmail.com>
2025-08-02 12:06:02 +0200

    feat: Add build and clean scripts for the xit project

---
+++ build_release.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# DEBUG
+
+# This script builds the complete xit project using xmake.
+# It assumes that xmake is installed and available in the PATH.
+# It builds the project in its subdirectories.
+# It also assumes that the xmakefile.json files are present in each subdirectory.
+
+# The order of the build is important, as all projects depend on others (except xit).
+
+echo "*********Building xit*********"
+xmake xit/xmakefile.json --config Release
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit. Exiting."
+    exit 1
+fi
+
+echo "*********Building xit.Timers*********"
+xmake xit.Timers/xmakefile.json --config Release
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Timers. Exiting."
+    exit 1
+fi
+
+echo "*********Building xit.IO*********"
+xmake xit.IO/xmakefile.json --config Release
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.IO. Exiting."
+    exit 1
+fi
+
+echo "*********Building xit.Input*********"
+xmake xit.Input/xmakefile.json --config Release
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Input. Exiting."
+    exit 1
+fi
+
+echo "*********Building xit.Clipboard*********"
+xmake xit.Clipboard/xmakefile.json --config Release
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Clipboard. Exiting."
+    exit 1
+fi
+
+echo "*********Building xit.Security*********"
+xmake xit.Security/xmakefile.json --config Release
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Security. Exiting."
+    exit 1
+fi
+
+echo "*********Building xit.Threading*********"
+xmake xit.Threading/xmakefile.json --config Release
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Threading. Exiting."
+    exit 1
+fi
+
+echo "*********Building xit.Application*********"
+xmake xit.Application/xmakefile.json --config Release
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Application. Exiting."
+    exit 1
+fi
+
+echo "*********Building xit.Account*********"
+xmake xit.Account/xmakefile.json --config Release
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Account. Exiting."
+    exit 1
+fi
+
+echo "*********Building xit.Window*********"
+xmake xit.Window/xmakefile.json --config Release
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Window. Exiting."
+    exit 1
+fi
+
+echo "*********Building xit.Drawing*********"
+xmake xit.Drawing/xmakefile.json --config Release
+
+# Check build result and return if it fails
+if [ $? -ne 0 ]; then
+    echo "Build failed for xit.Drawing. Exiting."
+    exit 1
+fi
